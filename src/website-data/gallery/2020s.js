be3a85e1fe264f7b1f5322423141835920485018
jbkad <jbkadibu@gmail.com>
2024-09-10 13:11:43 +0100

    update(gallery): new 2020s gallery images

---
+++ src/website-data/gallery/2020s.js
@@ -0,0 +1,26 @@
+export const images = [
+    {
+        src: require("../../assets/gallery/20s/2.jpg"),
+        caption: "Committee member Sam Manyak (2023/24) flying the 'Big Yellow Banana' Slingsby T.21 from the Lasham Gliding Heritage Centre"
+    },
+    {
+        src: require("../../assets/gallery/20s/1.jpg"),
+        caption: "2024 - icGC members helping to clean our trailers"
+    },
+    {
+        src: require("../../assets/gallery/20s/6.jpg"),
+        caption: "Annual Imperial College Gliding Club dinner 2023"
+    },
+    {
+        src: require("../../assets/gallery/20s/3.jpg"),
+        caption: "icGC Winter Tour 2022/23 - Gliding members getting air time in other ways after Wales weather didn't cooporate"
+    },
+    {
+        src: require("../../assets/gallery/20s/4.jpg"),
+        caption: "icGC Winter Tour 2022/23 - Member Joyce Kadibu full of smiles after a successful flight"
+    },
+    {
+        src: require("../../assets/gallery/20s/5.jpg"),
+        caption: "icGC Winter Tour 2022/23 - Members having dinner after a day of gliding"
+    },
+];
