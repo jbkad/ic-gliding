da88fe5ddbae07322e36a9a1ecb9ef6c6021f678
jbkad <jbkadibu@gmail.com>
2024-09-26 21:17:50 +0100

    update(committee): profile photos added

--- src/components/Committee.js
+++ src/components/Committee.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { data } from "../website-data/committeeMembers";
-import '../styles/components/committee.scss';
+import "../styles/components/committee.scss";
 
 export default function Committee() {
+  const handleEmptyProfile = (e) => {
+    e.target.src = "assets/committee/profile.jpeg";
+  };
 
-    const handleEmptyProfile = (e) => {
-        e.target.src = 'assets/committee/profile.jpeg';
-    };
-
-    return (
-        <div className="committee">
-            <div className="member">
-                {data.map((member, index) => (
-                    <div className="profile wallet" key={index}>
-                        <img 
-                            src={`assets/committee/${member.name}.png`}
-                            alt={member.name}
-                            className="headshot"
-                            onError={handleEmptyProfile}
-                        />
-                        <div className="name">{member.name}</div>
-                        <div className="position">{member.position}</div>
-                        <div className="course">{member.course}</div>
-                        <div className="year"> Year {member.year}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="committee">
+      <div className="member">
+        {data.map((member, index) => (
+          <div className="profile wallet" key={index}>
+            <img
+              src={`assets/committee/${member.name}.png`}
+              alt={member.name}
+              className="headshot"
+              onError={handleEmptyProfile}
+            />
+            <div className="name">{member.name}</div>
+            <div className="position">{member.position}</div>
+            <div className="course">{member.course}</div>
+            <div className="year"> Year {member.year}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
