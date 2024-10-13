433611768ed4dbe3a1666396597a9ea1fe529289
jbkad <jbkadibu@gmail.com>
2024-10-13 10:51:22 +0100

    style(introduction): card styling update

--- src/pages/what-is-gliding/WhatIsGliding.js
+++ src/pages/what-is-gliding/WhatIsGliding.js
@@ -27,38 +27,41 @@ export default function WhatIsGliding() {
         launch methods both take the glider from stationary on the ground to an
         altitude from which the glider can continue to fly.
       </p>
-      <h2 className="subheading">Winch launching</h2>
-      <p>
-        Winch launching the primary method of the launch used during training as
-        it is a low cost and rapid method of launch.
-      </p>
       <div className="card">
         <div className="container">
           <Winch />
-          <p>
-            The glider is attached using a cable to a powerful winch at the
-            opposite end of the runway. As the cable is pulled in the glider
-            will climb upwards at around 45° similar to a kite on a string. Once
-            at flying altitude the cable will be released from the glider and
-            the glider is free to fly around. The height achieved by a winch
-            launch is dependent on the weather conditions and the length of the
-            runway used to launch.
-          </p>
+          <div>
+            <h3 className="subheading">Winch launching</h3>
+            <p>
+              The glider is attached using a cable to a powerful winch at the
+              opposite end of the runway. As the cable is pulled in the glider
+              will climb upwards at around 45° similar to a kite on a string.
+              Once at flying altitude the cable will be released from the glider
+              and the glider is free to fly around. The height achieved by a
+              winch launch is dependent on the weather conditions and the length
+              of the runway used to launch.
+            </p>
+            <p>
+              Winch launching the primary method of the launch used during
+              training as it is a low cost and rapid method of launch.
+            </p>
+          </div>
         </div>
       </div>
-      <h2 className="subheading">Aerotow launching</h2>
-      <p>Aerotow launching is another method of launching gliders.</p>
       <div className="card">
         <div className="container">
           <Aerotow />
-          <p>
-            The glider is towed behind a powered aircraft using a rope until a
-            certain altitude is reached and the glider releases the rope. This
-            launch method is much more expensive and takes a lot longer to climb
-            to the desired altitude however it gives more flexibility than a
-            winch launch as the tow plane could climb as high as you could
-            reasonably want.
-          </p>
+          <div>
+            <h3 className="subheading">Aerotow launching</h3>
+            <p>
+              The glider is towed behind a powered aircraft using a rope until a
+              certain altitude is reached and the glider releases the rope. This
+              launch method is much more expensive and takes a lot longer to
+              climb to the desired altitude however it gives more flexibility
+              than a winch launch as the tow plane could climb as high as you
+              could reasonably want.
+            </p>
+          </div>
         </div>
       </div>
     </div>
