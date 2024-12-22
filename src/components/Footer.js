96ec742db7aaffb3831395a1f2715c596f55ee94
jbkad <jbkadibu@gmail.com>
2024-12-22 10:59:26 +0000

    feat(footer): layout update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,19 +11,8 @@ export default function Footer() {
       <div className="container">
         <div className="section-1">
           <div className="col-1">
-            <img
-              src={require("../assets/icgc.png")}
-              alt="Imperial College Gliding Club"
-              className="logo-icgc"
-            />
-            <div>
-              <div className="heading">Imperial College Gliding Club</div>
-              <div className="subheading">Est. 1930</div>
-            </div>
-          </div>
-          <div className="col-2">
             <div className="row-1">
-              <label>Top links</label>
+              <label>About Us</label>
               <Link
                 to="/whatisgliding/introduction"
                 onClick={navigateTo}
@@ -31,22 +20,6 @@ export default function Footer() {
                 aria-label="What is gliding?"
               >
                 What is gliding?
-              </Link>
-              <Link
-                to="/memberinformation/fleet-and-costs"
-                onClick={navigateTo}
-                className="link"
-                aria-label="Find out more about gliding costs"
-              >
-                Gliding costs
-              </Link>
-              <Link
-                to="/learningtoglide/join"
-                onClick={navigateTo}
-                className="link"
-                aria-label="Learn how to glide"
-              >
-                Learn how to glide
               </Link>
               <Link
                 to="/learningtoglide/location"
@@ -57,17 +30,6 @@ export default function Footer() {
                 Where do we fly?
               </Link>
               <Link
-                to="/gallery"
-                onClick={navigateTo}
-                className="link"
-                aria-label="Club gallery"
-              >
-                Club gallery
-              </Link>
-            </div>
-            <div className="row-2">
-              <label>Useful links</label>
-              <Link
                 to="/faqs"
                 onClick={navigateTo}
                 className="link"
@@ -75,6 +37,9 @@ export default function Footer() {
               >
                 FAQs
               </Link>
+            </div>
+            <div className="row-2">
+              <label>Useful Resources</label>
               <a
                 href="https://chat.whatsapp.com/EcanRrIn34KFmpIbJsSy4d"
                 className="link"
@@ -82,22 +47,6 @@ export default function Footer() {
               >
                 WhatsApp group
               </a>
-              <Link
-                to="/sitemap"
-                onClick={navigateTo}
-                className="link"
-                aria-label="Sitemap"
-              >
-                Sitemap
-              </Link>
-              <Link
-                to="/accessibility"
-                onClick={navigateTo}
-                className="link"
-                aria-label="Accessibility"
-              >
-                Accessibility
-              </Link>
               <a
                 href="https://glideandseek.com/"
                 className="link"
@@ -115,56 +64,51 @@ export default function Footer() {
                 Lasham Gliding
               </a>
             </div>
-          </div>
-        </div>
-        <hr />
-        <div className="section-2">
-          <div className="col-1">
-            <img
-              src={require("../assets/imperial.png")}
-              alt="Imperial College London"
-              className="logo-icl"
-            />
-            <div>
-              <a
-                className="note"
-                href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5033166/charity-overview"
-                target="blank"
+            <div className="row-3">
+              <label>Site information</label>
+              <Link
+                to="/sitemap"
+                onClick={navigateTo}
+                className="link"
+                aria-label="Sitemap"
               >
-                ©{currentYear} Imperial College Union. Registered Charity No.
-                1151241 | VAT Reg No. GB 240 5617 84
-              </a>
-              <a
-                className="address"
-                href="https://maps.app.goo.gl/8r8WdNV1TJXYMXqr7"
-                target="blank"
+                Sitemap
+              </Link>
+              <Link
+                to="/accessibility"
+                onClick={navigateTo}
+                className="link"
+                aria-label="Accessibility"
               >
-                Imperial College Union, Beit Quadrangle, Prince Consort Road,
-                London SW7 2BB
-              </a>
-              <div className="links">
-                <a
-                  className="link"
-                  href="https://icgliding.co.uk/"
-                  target="blank"
-                >
-                  icgliding.co.uk{" "}
-                </a>
-                <a
-                  className="link"
-                  href="https://imperialcollegeunion.org/"
-                  target="blank"
-                >
-                  imperialcollegeunion.org{" "}
-                </a>
-                <a
-                  className="link"
-                  href="https://imperial.ac.uk/"
-                  target="blank"
-                >
-                  imperial.ac.uk{" "}
-                </a>
-              </div>
+                Accessibility
+              </Link>
+            </div>
+            <div className="row-4">
+              <label>Top links</label>
+              <Link
+                to="/learningtoglide/join"
+                onClick={navigateTo}
+                className="link"
+                aria-label="Learn how to glide"
+              >
+                Learn how to glide
+              </Link>
+              <Link
+                to="/memberinformation/fleet-and-costs"
+                onClick={navigateTo}
+                className="link"
+                aria-label="Find out more about gliding costs"
+              >
+                Gliding costs
+              </Link>
+              <Link
+                to="/gallery"
+                onClick={navigateTo}
+                className="link"
+                aria-label="Club gallery"
+              >
+                Club gallery
+              </Link>
             </div>
           </div>
           <div className="col-2">
@@ -174,23 +118,89 @@ export default function Footer() {
                 target="blank"
                 aria-label="Instagram"
               >
-                {instagram_svg}
+                <span>{instagram_svg}</span>
               </a>
               <a
                 href="https://www.facebook.com/ImperialCollegeGlidingClub/"
                 target="blank"
                 aria-label="Meta"
               >
-                {facebook_svg}
+                <span>{facebook_svg}</span>
               </a>
               <a
                 href="mailto:gliding@imperial.ac.uk"
                 target="blank"
                 aria-label="E-mail"
               >
-                {mail_svg}
+                <span>{mail_svg}</span>
               </a>
             </div>
+          </div>
+        </div>
+        <hr />
+        <div className="section-2">
+          <div className="col-1">
+            <div className="footnote">
+              © {currentYear} Imperial College Union
+              <br />
+              <a
+                className="footnote"
+                href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5033166/charity-overview"
+                target="blank"
+              >
+                Registered Charity No. 1151241 | VAT Reg No. GB 240 5617 84
+              </a>
+            </div>
+            <a
+              className="address"
+              href="https://maps.app.goo.gl/8r8WdNV1TJXYMXqr7"
+              target="blank"
+            >
+              Imperial College Union, Beit Quadrangle, Prince Consort Road,
+              London SW7 2BB
+            </a>
+            <div className="links">
+              <a
+                className="link"
+                href="https://icgliding.co.uk/"
+                target="blank"
+              >
+                icgliding.co.uk{" "}
+              </a>
+              <a
+                className="link"
+                href="https://imperialcollegeunion.org/"
+                target="blank"
+              >
+                imperialcollegeunion.org{" "}
+              </a>
+              <a className="link" href="https://imperial.ac.uk/" target="blank">
+                imperial.ac.uk{" "}
+              </a>
+            </div>
+          </div>
+          <div className="col-2">
+            <a href="https://imperial.ac.uk/" target="blank">
+              <img
+                src={require("../assets/imperial-logo.png")}
+                alt="Imperial College London"
+                className="logo-icl"
+              />
+            </a>
+            <a href="https://gliding.co.uk/" target="blank">
+              <img
+                src={require("../assets/bga.png")}
+                alt="British Gliding Association"
+                className="logo-bga"
+              />
+            </a>
+            <a href="https://icgliding.co.uk/" target="blank">
+              <img
+                src={require("../assets/icgc.png")}
+                alt="Imperial College Gliding Club"
+                className="logo-icgc"
+              />
+            </a>
           </div>
         </div>
       </div>
