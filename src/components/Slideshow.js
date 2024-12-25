8e811fd0605ba7c15918da0677b97dc11dc771e2
jbkad <jbkadibu@gmail.com>
2024-12-25 20:51:56 +0000

    feat(slideshow): new pics added

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,81 +1,73 @@
 import React, { useEffect, useState } from "react";
-import '../styles/components/slideshow.scss';
+import "../styles/components/slideshow.scss";
 
-const GalleryItem = ({ children, width, image}) => {
-    return (
-        <div 
-            className="item"
-            style={{ width: width}}
-        >
-            <img 
-                src={image}
-                alt="Imperial College Gliding Club"
-            />
-            {children}
-        </div>
-    )
-}
+const GalleryItem = ({ children, width, image }) => {
+  return (
+    <div className="item" style={{ width: width }}>
+      <img src={image} alt="Imperial College Gliding Club" />
+      {children}
+    </div>
+  );
+};
 
 const Carousel = ({ children }) => {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const updateIndex = (newIndex) => {
-        if (newIndex < 0) {
-            newIndex = React.Children.count(children) - 1;
-        } else if (newIndex >= React.Children.count(children)) {
-            newIndex = 0;
-        }
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
 
-        setActiveIndex(newIndex);
+    setActiveIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateIndex(activeIndex + 1);
+    }, 3500);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
     };
+  });
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            (updateIndex(activeIndex + 1));
-        }, 3500);
-
-        return () => {
-            if (interval) {
-                clearInterval(interval);
-            }
-        };
-    });
-
-    return (
-        <div className="slideshow">
-            <div
-                className="container"
-                style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-            >
-                {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "100%" });
-                })}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="slideshow">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+      </div>
+    </div>
+  );
+};
 
 const galleryImg = [
-    "/assets/slideshow/gliding-1.jpg",
-    "/assets/slideshow/gliding-2.jpg",
-    "/assets/slideshow/gliding-3.jpg",
-    "/assets/slideshow/gliding-4.jpg",
-    "/assets/slideshow/gliding-5.jpg",
-    "/assets/slideshow/gliding-6.jpg",
-    "/assets/slideshow/gliding-7.jpg"
+  "/assets/slideshow/gliding-1.jpg",
+  "/assets/slideshow/gliding-2.jpg",
+  "/assets/slideshow/gliding-3.jpg",
+  "/assets/slideshow/gliding-4.jpg",
+  "/assets/slideshow/gliding-5.jpg",
+  "/assets/slideshow/gliding-6.jpg",
+  "/assets/slideshow/gliding-7.jpg",
+  "/assets/slideshow/gliding-8.jpg",
 ];
 
 function Slideshow() {
-    return (
-        <Carousel>
-            {galleryImg.map((image, index) => (
-                <GalleryItem 
-                    key={index}
-                    image={image} 
-                />
-            ))}
-        </Carousel>
-    );
+  return (
+    <Carousel>
+      {galleryImg.map((image, index) => (
+        <GalleryItem key={index} image={image} />
+      ))}
+    </Carousel>
+  );
 }
 
 export { Slideshow };
