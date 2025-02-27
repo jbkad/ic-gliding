ece72daf977281bdbbdaf61be8e8993de1d1f4e3
jbkad <jbkadibu@gmail.com>
2025-02-27 23:33:21 +0000

    feat(costs): table view amended

--- src/components/CostsTable.js
+++ src/components/CostsTable.js
@@ -2,59 +2,55 @@ import React from "react";
 import "../styles/components/costs-table.scss";
 
 export default function CostsTable() {
-
-    return (
-        <table className="costs">
-            <tbody>
-                <tr>
-                    <td className="heading">Membership</td>
-                    <td>£50</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td className="heading">Winch Launch</td>
-                    <td>£10</td>
-                    <td>
-                        <div>• 10 minute flight off a winch: £13. The vast majority of your training will cost this. </div>
-                        <div>• 30 minute flight off a winch: £19</div>
-                        <div>• 1 hour flight off a winch: £28 </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td className="heading">Aerotow Launch</td>
-                    <td>£43 <div className="note"> to 2000ft</div> </td>
-                    <td>20 minute flight from a 2000ft aerotow: £49 </td>
-                </tr>
-                <tr>
-                    <td className="heading">Soaring Fees</td>
-                    <td>
-                        <div>£30p/min</div>
-                        <div className="note">Minimum charge: 7 mins</div> 
-                        <div className="note">Maximum charge: 2.5 hrs</div>
-                    </td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td className="heading">Weekend Trip Fee: Train</td>
-                    <td>£12</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td className="heading">Weekend Trip Fee: Minibus</td>
-                    <td>£16</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td className="heading">Student Daily Hire</td>
-                    <td>£30</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td className="heading">Alumni Daily Hire</td>
-                    <td>£35</td>
-                    <td>-</td>
-                </tr>
-            </tbody>
-        </table>
-    );
+  return (
+    <table className="costs">
+      <tbody>
+        <tr>
+          <td className="heading">Membership</td>
+          <td>£50</td>
+        </tr>
+        <tr>
+          <td className="heading">Winch Launch</td>
+          <td>
+            <div>£10</div>
+            <div className="note">
+              • 10 min flight = £13 (Majority of your training will cost this)
+            </div>
+            <div className="note">• 30 min flight = £19</div>
+            <div className="note">• 1 hour flight = £28</div>
+          </td>
+        </tr>
+        <tr>
+          <td className="heading">Aerotow Launch</td>
+          <td>
+            £43 <div className="note"> to 2000ft</div>{" "}
+          </td>
+        </tr>
+        <tr>
+          <td className="heading">Soaring Fees</td>
+          <td>
+            <div>£30p/min</div>
+            <div className="note">Min charge: 7 mins</div>
+            <div className="note">Max charge: 2.5 hrs</div>
+          </td>
+        </tr>
+        <tr>
+          <td className="heading">Weekend Trip Fee: Train</td>
+          <td>£12</td>
+        </tr>
+        <tr>
+          <td className="heading">Weekend Trip Fee: Minibus</td>
+          <td>£16</td>
+        </tr>
+        <tr>
+          <td className="heading">Student Daily Hire</td>
+          <td>£30</td>
+        </tr>
+        <tr>
+          <td className="heading">Alumni Daily Hire</td>
+          <td>£35</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
